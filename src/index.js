996af5d702e3e8c0ab8f8f1bf1137f91b19e6597
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-08-24 19:12:03 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let taskForm = document.querySelector('form')
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newItem = document.getElementById('new-task-description').Value;
+    console.log(newItem);
+    buildTask(newItem);
+  })
+
+  function buildTask(task){
+    const li = document.createElement('li')
+    li.textContent= `${task}`
+    const theList = document.querySelector('#tasks')
+    theList.appendChild(li)
+  
+
+const button = document.createElement('button')
+button.addEventListener('click',handledelete);
+button.textContent = 'x'
+li.textContent = `${task}`
+li.appendChild(button)
+console.log(li)
+theList.appendChild(li)
+  }
 });
+
+
+
+function handledelete(e){
+  e.target.parentNode.remove();
+}
+
